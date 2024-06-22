6a0369396af778e4342cff38c2164ccc9c4ee61f
cryptomathematician <bosman006@st.ug.edu.gh>
2024-06-22 00:56:25 +0000

    Added the sent, receive,loan and topup functionality

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { ScrollView, StyleSheet, Text, View, Image, TextInput, FlatList, Button } from 'react-native';
 import Profiles from './components/profiles';
 import Cards from './components/cards';
+import Payments from './components/payments';
 
 export default function App() {
   return (
     <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer} >
        <Profiles />
        <Cards />
+       <Payments/>
     </ScrollView>
   );
 }
---
+++ components/payments.js
@@ -0,0 +1,57 @@
+import { ScrollView, StyleSheet, Text, View, Image, TextInput, FlatList, Button } from 'react-native';
+import React from 'react';
+
+
+
+function Payments() {
+  return (
+    <View style={styles.searchBox}>
+        <View style={[styles.searchContainer, { marginRight: 12 }]}>
+            <Image source={require('../assets/send.png')} style={[styles.searchImage]} />
+            <Text style={styles.textType}>Sent</Text>
+        </View>
+        <View style={[styles.searchContainer, { marginHorizontal: 12}]}>
+            <Image source={require('../assets/recieve.png')} style={[styles.searchImage]}/>
+            <Text style={styles.textType}>Receive</Text>
+        </View >
+        <View style={[styles.searchContainer, { marginHorizontal: 12 }]}>
+            <Image source={require('../assets/loan.png')} style={[styles.searchImage]} />
+            <Text style={styles.textType}>Loan</Text>
+        </View>
+        <View style={[styles.searchContainer, { marginRight: 12}]}>
+            <Image source={require('../assets/topUp.png')} style={[styles.searchImage]} />
+            <Text style={styles.textType}>Topup</Text>
+        </View>
+    </View>
+    
+  )
+}
+
+const styles = StyleSheet.create({
+    searchContainer: {
+        right: 70,
+        top: 20,
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        backgroundColor: '#f0f0f0', // adjust the color as needed
+        alignItems: 'center',
+      },
+    searchImage: {
+        width: 24,
+        height: 30,
+        top: 18
+        
+    } ,
+    searchBox: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginLeft: 150,
+        top: 5
+    },
+    textType : {
+        top: 25
+    }
+});
+
+export default Payments
